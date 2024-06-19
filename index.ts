90857ee9353be4709df7a3c64d247660e5b78e4a
tiago-clima <casmei@protonmail.com>
2024-06-19 08:47:18 -0300

    feat(api): limpa os dados monta visualizacao

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import ColetorDeDados from "./src/componentes/coletor-de-dados/_index";
 
-const gastos = ColetorDeDados.gastos(6, 2024, 3);
+const gastos = await ColetorDeDados.gastos(6, 2024);
+
+console.log(gastos?.getEmpenhosDoDia());
+console.log(gastos?.maiorFavorecido());
+console.log(gastos?.somaTotalDeGastos());
--- src/componentes/coletor-de-dados/ColetorDeDados.ts
+++ src/componentes/coletor-de-dados/ColetorDeDados.ts
@@ -1,32 +1,55 @@
 import type { IApi } from "./contrato/IApi";
 import type { Empenho } from "./tipos/empenhos.type";
+import _ from "lodash";
 
 export default class ColetorDeDados {
   private buscar: IApi;
+  private empenhos: Empenho[] | null = null;
+  private diaDaBusca: number = new Date().getDate();
 
   constructor(dataFetcher: IApi) {
     this.buscar = dataFetcher;
   }
 
-  async gastos(mes: number, ano: number, dia?: number): Promise<void> {
+  async gastos(mes: number, ano: number, dia?: number) {
     try {
       const empenhos = await this.buscar.gastos(mes, ano);
-      const empenhosDoDia = this.getEmpenhosDoDia(empenhos, dia);
+      this.empenhos = empenhos;
 
-      console.log(empenhosDoDia);
+      if (dia) this.diaDaBusca = dia;
+
+      return this;
     } catch (error) {
       console.error("Erro ao buscar os empenhos favorecidos:", error);
     }
   }
 
-  private getEmpenhosDoDia(empenhos: Empenho[], dia?: number): Empenho[] {
-    const diaEscolhido = dia ?? new Date().getDate();
+  public getEmpenhosDoDia(dia?: number) {
+    const diaEscolhido = dia ?? this.diaDaBusca;
 
-    const empenhosDoDia = empenhos.filter((empenho) => {
+    const empenhosDoDia = this.empenhos?.filter((empenho) => {
       const dataEmpenho = new Date(empenho.data);
       return dataEmpenho.getDate() === diaEscolhido;
     });
 
     return empenhosDoDia;
+  }
+
+  public somaTotalDeGastos() {
+    return this.empenhos?.reduce((soma, empenho) => {
+      return soma + empenho.valor;
+    }, 0);
+  }
+
+  public maiorFavorecido() {
+    return _.chain(this.empenhos)
+      .groupBy("nome_favorecido")
+      .map((items, chave) => ({
+        nome: chave,
+        cpf: items[0].documento_favorecido,
+        valor: _.sumBy(items, "valor"),
+      }))
+      .orderBy("valor", "desc")
+      .value();
   }
 }
